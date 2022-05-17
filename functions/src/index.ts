e5972eda03cee4566623eb3dbab99095468f9817
Claire Utrecht <utrechtc@gmail.com>
2022-05-16 21:38:18 -0400

    pushing back end folder

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from 'firebase-functions';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+ 
+export const api = functions.https.onRequest(app);
+
+//this came directly from slide 79 in the Modern Web slides. If you think or find that it is missing something, you might start by referencing that slide.
+
